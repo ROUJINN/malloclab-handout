9d69d52872706d713d2774da6a3701bc1eb3c3c7
ROUJIN <18912678466@163.com>
2023-12-07 23:16:00 +0800

    还在改4字节指针，下一步我本来打算size_t全变为unsigned，但是不用size_t会没法检查malloc调用是否超了堆的大小，所以不行

--- mm.c
+++ mm.c
@@ -38,55 +38,82 @@
 #define DSIZE       8       /* Double word size (bytes) */
 #define CHUNKSIZE  (1<<12)  /* Extend heap by this amount (bytes) */ 
 
+#define MAX(x, y) ((x) > (y)? (x) : (y)) 
+
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(p) (((size_t)(p) + (ALIGNMENT-1)) & ~0x7)
 
-#define GET(p)       (*(unsigned int *)(p)) /*这是取了指针指向的内容的值*/
-#define PUT(p, val)  (*(unsigned int *)(p) = (val))
+ 
+static unsigned long base = (*(unsigned long *)mem_heap_lo()) & 0x00000000;
+
+/* 给一个unsigned的指针返回unsigned long */
+#define EXTEND_PTR(p) (base + (unsigned long)p) 
+
+/* 拿void*指针的后32位作为unsigned，这里直接类型转换就是取后32位 */
+#define SHRINK_PTR(p) ((unsigned)(p))
+
+/*返回一个unsigned*/
+#define GET(p)       (*(unsigned *)(EXTEND_PTR(p))) 
+
+/*给unsigned的指针，往里放东西*/
+#define PUT(p, val)  (*(unsigned *)(EXTEND_PTR(p)) = (val))
 #define PACK(size, alloc)  ((size) | (alloc))
 
-#define CUR_ALLOC(p) (GET(p) & 0x1) /*1代表已经分配*/
+#define CUR_ALLOC(p) (GET(p) & 0x1)
 
-#define BP2P(bp) ((char *)(bp) - WSIZE) /*由bp返回header的指针p*/
-#define BP2FP(bp) ((char *)(bp) + GET_SIZE(BP2P(bp)) - DSIZE)
-#define FP2BP(fp) ((char *)(fp) - GET_SIZE(fp) + DSIZE)
-#define PP2BP(pp) ((char *)(pp) - WSIZE) 
-#define BP2PP(bp) ((char *)(bp) + WSIZE)
+/*由bp返回header的指针p，注意这里返回的是unsigned*/
+#define BP2P(bp) ((bp) - WSIZE) 
+#define BP2FP(bp) ((bp) + GET_SIZE(BP2P(bp)) - DSIZE)
+#define FP2BP(fp) ((fp) - GET_SIZE(fp) + DSIZE)
+#define PP2BP(pp) ((pp) - WSIZE) 
+#define BP2PP(bp) ((bp) + WSIZE)
 
+/*size是asize，包含头尾，类型是unsigned*/
+#define GET_SIZE(p)  (GET(p) & ~0x7)  
 
-#define GET_SIZE(p)  (GET(p) & ~0x7)  /*这个返回的size是asize，包含头尾*/
-
+/* 返回unsigned */
 #define LINK_NEXT_BP(bp)  (GET(bp))
 #define LINK_PREV_PP(pp)  (GET(pp))
 
-#define MEM_PREV_ALLOC(p) ((GET(p) & 0b10) >> 1) /*MEM代表是物理内存上的前一个*/
-#define MEM_NEXT_BP(bp) ((char*)(bp) + GET_SIZE(BP2P(bp)))
-#define MEM_PREV_FP(bp) ((char*)(bp) - DSIZE) /*FP代表脚部指针，需要这个时前一个必定空闲，从而有脚部*/
+/*MEM代表是物理内存上的前一个*/
+#define MEM_PREV_ALLOC(p) ((GET(p) & 0b10) >> 1) 
 
+#define MEM_NEXT_BP(bp) ((bp) + GET_SIZE(BP2P(bp)))
 
+/*FP代表脚部指针，需要这个时前一个必定空闲，从而有脚部*/
+#define MEM_PREV_FP(bp) ((bp) - DSIZE) 
 
-static char *root = 0; /*prol的next*/
+// static void place(void *bp, size_t asize);
+// static void *find_fit(size_t asize);
+// static void *extend_heap(size_t words);
+// static void *coalesce(void *bp);
+// static void link_insert(void *bp);
+// static void link_LIFOinsert(void *bp);
+// static void link_delete(void *bp);
 
+void mm_checkheap(int lineno);
+
+static void *root = 0; /*prol的next*/
 
 /*
  * Initialize: return -1 on error, 0 on success.
  */
 int mm_init(void) 
 {
-    
-
-    /* Create the initial empty heap */
     root = mem_sbrk(6*WSIZE);
-    PUT(root,PACK(0,0b01)); /* prologue header */
-    PUT(root+WSIZE,root+3*WSIZE); /* prologue bp */
-    PUT(root+2*WSIZE,NULL); /* prologue pp */
-    PUT(root+WSIZE,PACK(0,0b11)); /* epilogue header*/
-    PUT(root+WSIZE,NULL); /* epilogue bp*/
-    PUT(root+WSIZE,root+2*WSIZE); /* epilogue pp*/
+    unsigned tmp_root = SHRINK_PTR(root);
+
+    PUT(tmp_root,PACK(0,0b01)); /* prologue header */
+    PUT(tmp_root+WSIZE,tmp_root+3*WSIZE); /* prologue bp */
+    PUT(tmp_root+2*WSIZE,NULL); /* prologue pp */
+    PUT(tmp_root+WSIZE,PACK(0,0b11)); /* epilogue header*/
+    PUT(tmp_root+WSIZE,NULL); /* epilogue bp*/
+    PUT(tmp_root+WSIZE,tmp_root+2*WSIZE); /* epilogue pp*/
 
     root = root + WSIZE;
 
     extend_heap(CHUNKSIZE/WSIZE);
+    return 0;
 }
 
 /*
@@ -96,8 +123,9 @@ void *malloc (size_t size) {
 
     size_t asize;
     size_t extend_size;
+    void *bp;
 
-    if (heap_listp == 0){
+    if (root == 0){
         mm_init();
     }
     
@@ -119,8 +147,8 @@ void *malloc (size_t size) {
     }
 
     /* No fit found. Get more memory and place the block */
-    extendsize = MAX(asize,CHUNKSIZE);                 
-    if ((bp = extend_heap(extendsize/WSIZE)) == NULL)  
+    extend_size = MAX(asize,CHUNKSIZE);                 
+    if ((bp = extend_heap(extend_size/WSIZE)) == NULL)  
         return NULL;                                  
     place(bp, asize);                                 
     return bp;
@@ -147,22 +175,23 @@ void free(void *bp) {
  * realloc - you may want to look at mm-naive.c
  */
 void *realloc(void *oldptr, size_t size) {
-    size_t oldsize;
-    void *newptr;
+    // size_t oldsize;
+    // void *newptr;
 
-    /* If size == 0 then this is just free, and we return NULL. */
-    if(size == 0) {
-        free(oldptr);
-        return 0;
-    }
+    // /* If size == 0 then this is just free, and we return NULL. */
+    // if(size == 0) {
+    //     free(oldptr);
+    //     return 0;
+    // }
 
-    /* If oldptr is NULL, then this is just malloc. */
-    if(oldptr == NULL) {
-        return malloc(size);
-    }
-    return NULL;
+    // /* If oldptr is NULL, then this is just malloc. */
+    // if(oldptr == NULL) {
+    //     return malloc(size);
+    // }
+    // return NULL;
 
     /*要什么复制数据之类的，先不管*/
+    return NULL;
 }
 
 /*
@@ -202,28 +231,28 @@ static int aligned(const void *p) {
  */
 void mm_checkheap(int lineno) {
 
-    void *bp,*heap_lo,*heap_hi;
+    // void *bp,*heap_lo,*heap_hi;
 
-    heap_lo = mem_heap_lo();
-    heap_hi = mem_heap_hi();
+    // heap_lo = mem_heap_lo();
+    // heap_hi = mem_heap_hi();
 
-    /*Check epilogue blocks.*/
-    if (GET(heap_listp) != PACK(0,1)) {
-        printf("epilogue error in line %d\n",lineno);
-    }
+    // /*Check epilogue blocks.*/
+    // if (GET(heap_listp) != PACK(0,1)) {
+    //     printf("epilogue error in line %d\n",lineno);
+    // }
 
-    /*Check each block’s address alignment and .*/
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-        if (!aligned((void*)bp)) {
-            printf("pointer not aligned in line %d\n",lineno);
-        }
-        if (!in_heap((void*)bp)) {
-            printf("pointer not in heap in line %d\n",lineno);
-        }
-    }
+    // /*Check each block’s address alignment and .*/
+    // for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+    //     if (!aligned((void*)bp)) {
+    //         printf("pointer not aligned in line %d\n",lineno);
+    //     }
+    //     if (!in_heap((void*)bp)) {
+    //         printf("pointer not in heap in line %d\n",lineno);
+    //     }
+    // }
 }
 
-static void *link_delete(void *bp) {
+static void link_delete(void *bp) {
     
     void *prev_pp = LINK_PREV_PP(BP2PP(bp));
     void *prev_bp = PP2BP(prev_pp);
@@ -232,9 +261,10 @@ static void *link_delete(void *bp) {
 
     PUT(prev_bp,next_bp);
     PUT(next_pp,prev_pp);
+    return;
 }
 
-static void *link_LIFOinsert(void *bp) {
+static void link_LIFOinsert(void *bp) {
     void *next_bp = LINK_NEXT_BP(root);
     void *next_pp = BP2PP(next_bp);    
     void *prev_bp = root;
@@ -244,9 +274,10 @@ static void *link_LIFOinsert(void *bp) {
     PUT(bp,next_bp);
     PUT(BP2PP(bp),prev_pp);
     PUT(next_pp,BP2PP(bp));
+    return;
 }
 
-static void *link_insert(void *bp) {
+static void link_insert(void *bp) {
     void *next_bp = LINK_NEXT_BP(bp);
     void *next_pp = BP2PP(next_bp);    
     void *prev_pp = LINK_PREV_PP(BP2PP(bp));
@@ -256,13 +287,14 @@ static void *link_insert(void *bp) {
     PUT(bp,next_bp);
     PUT(BP2PP(bp),prev_pp);
     PUT(next_pp,BP2PP(bp));
+    return;
 }
 
 static void *coalesce(void *bp) {
     unsigned prev_alloc = MEM_PREV_ALLOC(BP2P(bp));
     unsigned next_alloc = CUR_ALLOC(BP2P(MEM_NEXT_BP(bp)));
     unsigned prev_prev_alloc = MEM_PREV_ALLOC(MEM_PREV_FP(bp));
-    size_t size = GET_SIZE(BP2P(p));
+    size_t size = GET_SIZE(BP2P(bp));
 
     if (prev_alloc && next_alloc) {
         link_LIFOinsert(bp);
@@ -289,6 +321,7 @@ static void *coalesce(void *bp) {
         link_delete(bp);
         PUT(BP2P(bp),PACK(size, 2*prev_prev_alloc));
         link_LIFOinsert(bp);
+        return bp;
     }
 
     /* 前后都合并 */
@@ -310,12 +343,12 @@ static void *extend_heap(size_t words) {
     /* Allocate an even number of words to maintain alignment */
     size_t size = (words % 2) ? (words+1) * WSIZE : words * WSIZE; 
 
-    char *temp_p = mem_sbrk(size);
-    char *bp = temp_p - DSIZE;
-    char *epil_bp = bp + size;
+    void *temp_p = mem_sbrk(size);
+    void *bp = temp_p - DSIZE;
+    void *epil_bp = bp + size;
     unsigned prev_alloc = MEM_PREV_ALLOC(BP2P(bp)); /* 看epil的MEM上前一个是否alloc */
-    char *prev_pp = LINK_PREV_PP(BP2PP(bp));
-    char *prev_bp = PP2BP(bp);
+    void *prev_pp = LINK_PREV_PP(BP2PP(bp));
+    void *prev_bp = PP2BP(bp);
     
     /*新的epil*/
     PUT(epil_bp,NULL);
@@ -333,7 +366,7 @@ static void *extend_heap(size_t words) {
 }
 
 /* first fit */
-static void find_fit(size_t asize) {
+static void* find_fit(size_t asize) {
     void *bp;
     for (bp = root; GET_SIZE(BP2P(bp)) > 0; bp = LINK_NEXT_BP(bp)) {
         if (asize <= GET_SIZE(BP2P(bp))) {
@@ -351,7 +384,7 @@ static void place(void *bp, size_t asize) {
         PUT(BP2P(bp), PACK(asize, 2*prev_alloc));
         bp = MEM_NEXT_BP(bp);
         PUT(BP2P(bp), PACK(csize-asize,0b10));
-        PUT(BP2FP(bp), PACK(csize-asize),0b10);
+        PUT(BP2FP(bp), PACK(csize-asize,0b10));
     }
 
     else {
